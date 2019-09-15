ec12334aab551ecbe3d7691344f6b3a451cd1ed8
Tobias Casper <tobias.casper@gmail.com>
2019-09-15 23:25:30 +0200

    chore: Lower min required Elixir version to 1.7

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tint.MixProject do
     [
       app: :tint,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
