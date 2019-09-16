d5922546847eeb317c67f3c568e9980de3dd589c
Tobias Casper <tobias.casper@gmail.com>
2019-09-16 21:01:58 +0200

    improve(test): Add test for `Tint.RGB.human_euclidean_distance/2`

--- test/tint/rgb_test.exs
+++ test/tint/rgb_test.exs
@@ -224,7 +224,20 @@ defmodule Tint.RGBTest do
   end
 
   describe "human_euclidean_distance/2" do
-    test "get Euclidean distance with factors optimized for human perception"
+    test "get Euclidean distance with weights optimized for human perception" do
+      Enum.each(
+        [
+          {~K[127,0,255]r, ~K[#FFF], {2, 4, 3}},
+          {~K[127,255,0]r, ~K[#000], {2, 4, 3}},
+          {~K[128,0,255]r, ~K[#FFF], {3, 4, 2}},
+          {~K[128,255,0]r, ~K[#000], {3, 4, 2}}
+        ],
+        fn {color, other_color, weights} ->
+          assert RGB.human_euclidean_distance(color, other_color) ==
+                   RGB.euclidean_distance(color, other_color, weights: weights)
+        end
+      )
+    end
   end
 
   describe "nearest/2" do
