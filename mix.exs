757051a1f339891ff69549f8b06033b9fb93de32
Tobias Casper <tobias.casper@gmail.com>
2025-02-03 11:12:34 +0100

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Tint.MixProject do
   def project do
     [
       app: :tint,
-      version: "1.2.0",
+      version: "1.3.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
